028cb57be3ca2458c982e1e30e3465cfd1ff071c
PeterMagdyIskander <PeterMagdyIskander@gmail.com>
2021-05-16 01:39:14 +0200

    literally copy pasted someone's work need to understand it tmr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { AiOutlineComment } from 'react-icons/ai';
+import React,{useState} from 'react';
+
+
+function CreatePost(props){
+  
+  const [data,setData]=useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setData((prevData) => {
+      return {
+        ...prevData,
+        [name]: value
+      };
+    });
+  }
+  function submitPost(event) {
+    props.onAdd(data);
+    setData({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+  return(
+    <div>
+    <form>
+    <input
+        name="title"
+        onChange={handleChange}
+        value={data.title}
+        placeholder="Title"
+        />
+
+      <textarea
+        name="content"
+        onChange={handleChange}
+        value={data.content}
+        placeholder="Take a note..."
+      />
+      <button onClick={submitPost}></button>
+    </form>
+  </div> 
+  )
 }
 
+
+function Post(props){
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+  return(
+    <div className="post">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>
+      </button>
+    </div>
+  )
+}
+
+
+function App() {
+  const [data, setData] = useState([]);
+
+  function addData(newData) {
+    setData((prevData) => {
+      return [...prevData, newData];
+    });
+  }
+
+  function deleteData(id){
+    setData((prevData)=>{
+      return prevData.filter((dataItem,index)=>{
+        return index!==id;
+      });
+    });
+  }
+
+  
+  return (
+    <div className="App">
+    <CreatePost onAdd={addData} />
+      {data.map((dataItem,index)=>{
+          return(
+            <Post
+              key={index}
+              id={index}
+              title={dataItem.title}
+              content={dataItem.content}
+              onDelete={deleteData}
+            />
+          )
+        })}
+    </div>
+  )
+}
+
+
 export default App;
+
+
+
+
+/*
+<input className="btn" type="text" onChange={getData}></input>
+      <div > <AiOutlineComment /><button  onClick={()=>setPost(true)}
+      >Post</button></div>
+       */
